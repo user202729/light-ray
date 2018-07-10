f7f7f10759456554a40d59b05c72b5ff072680ae
user202729 <25191436+user202729@users.noreply.github.com>
2018-07-10 11:49:14 +0700

    computePolygons can make different colored edges different now

--- Main.java
+++ Main.java
@@ -14,11 +14,10 @@ import java.util.ArrayList;
 import static java.util.Arrays.asList;
 import static java.lang.System.out;
 
-
-class MyJPanel extends JPanel {
+class Panel extends JPanel {
 	BufferedImage background;
 
-	MyJPanel(BufferedImage background){
+	Panel(BufferedImage background){
 		this.background=background;
 	}
 	
@@ -28,12 +27,113 @@ class MyJPanel extends JPanel {
 	}
 }
 
+class CircularList<E>{ // linked list
+	// Should this extends AbstractSequentialList?
+	class Node{
+		E value;
+		Node next;
+		Node(E v){value=v;}
 
+		void addAfter(E v){
+			Node next2=next;
+			next=new Node(v);
+			next.next=next2;
+		}
+	}
+	Node item; // any item. Not guaranteed to be fixed.
+	CircularList(E x){item=new Node(x);item.next=item;}
 
-public class Main{
-	// default configuration. May be used for debugging.
-	static String suffix="bmp",path="/home/user202729/test.bmp";
-	static boolean antialias=false;
+	void removeAfter(Node node){
+		// this method must not be provided as a member method
+		// because it may invalidate (item).
+		if(node.next==item)item=node;
+		if(node==node.next)throw new RuntimeException(
+			"Attempt to remove on a singleton circular list");
+		node.next=node.next.next;
+	}
+
+	int computeLength(){
+		int len=0;
+		Node node=item;do{
+			++len;
+			node=node.next;
+		}while(node!=item);
+		return len;
+	}
+}
+
+final class Point implements Comparable<Point>{
+	final int x,y;
+	Point(int _x,int _y){x=_x ;y=_y ;}
+
+	Point add(Point p){ return new Point(x+p.x,y+p.y); }
+	Point sub(Point p){ return new Point(x-p.x,y-p.y); }
+	Point mul(int a){ return new Point(x*a,y*a); }
+	Point shr(int a){ return new Point(x>>a,y>>a); }
+
+	Point neg (){ return new Point(-x,-y); }
+	/// Rotate 90° clockwise, assuming +x is →, +y is ↓.
+	Point rt90(){ return new Point(-y, x); }
+	Point lt90(){ return new Point( y,-x); }
+
+	int dot  (Point p){ return x*p.x + y*p.y; }
+	/// Magnitude of the resulting vector when interpret the two
+	/// points as vector in 3D space and take the cross product.
+	int cross(Point p){ return x*p.y - y*p.x; }
+
+	int norm(){ return x*x+y*y; } // dot with itself
+	double dist(Point p){ return Math.sqrt(sub(p).norm()); }
+
+	/// Return the distance from this point to line AB.
+	double distToLine(Point a,Point b){
+		Point ab=b.sub(a);
+		int norm=ab.norm();
+		assert norm>0;
+		return Math.abs(sub(a).cross(ab)/Math.sqrt(norm));
+	}
+
+	/// Return the distance from this point to line segment AB.
+	double distToSegment(Point a,Point b){
+		Point ab=b.sub(a);
+		if(sub(a).dot(ab)<=0)return dist(a);
+		if(sub(b).dot(ab)>=0)return dist(b);
+		return distToLine(a,b);
+	}
+
+	@Override
+	public int compareTo(Point p){
+		return x!=p.x?
+			Integer.compare(x,p.x):Integer.compare(y,p.y);
+	}
+
+	@Override
+	public boolean equals(Object o){
+		if(o==null||!(o instanceof Point))return false;
+		Point p=(Point)o;
+		return x==p.x&&y==p.y;
+	}
+	@Override
+	public int hashCode(){ return x*109+y; }
+	@Override
+	public String toString(){ return "("+x+", "+y+')'; }
+}
+
+enum Color{
+	Comment				(0x000000),
+	Background			(0xffffff),
+	Mirror				(0x808080),
+	Pop					(0xff0000),
+	NegateOrDivide		(0xff6a00),
+	Swap				(0xffd800),
+	IncrementOrAdd		(0x00ffff),
+	Align				(0xb200ff),
+	Push				(0x0026ff),
+	DoubleOrMultiply	(0x00ff21),
+	Unused				(0x7f0000),
+	Unused2				(0xff00dc);
+
+	public final int value;
+	Color(int value){this.value=value;}
 
 	/// Input format: RGB. Return sum of squared distances.
 	static int colorDist(int a,int b){
@@ -52,24 +152,14 @@ public class Main{
 		return new java.awt.Color(rgb)
 			.brighter().brighter().brighter().getRGB();
 	}
+}
 
-	static enum Color{
-		Comment				(0x000000),
-		Background			(0xffffff),
-		Mirror				(0x808080),
-		Pop					(0xff0000),
-		NegateOrDivide		(0xff6a00),
-		Swap				(0xffd800),
-		IncrementOrAdd		(0x00ffff),
-		Align				(0xb200ff),
-		Push				(0x0026ff),
-		DoubleOrMultiply	(0x00ff21),
-		Unused				(0x7f0000),
-		Unused2				(0xff00dc);
 
-		public final int value;
-		Color(int value){this.value=value;}
-	}
+
+public class Main{
+	// default configuration. May be used for debugging.
+	static String suffix="bmp",path="/home/user202729/test.bmp";
+	static boolean antialias=false;
 
 	static Color[][] data;
 	static int X,Y;
@@ -101,101 +191,91 @@ public class Main{
 		}
 	}
 
-	static class CircularList<E>{ // linked list
-		// Should this extends AbstractSequentialList?
-		class Node{
-			E value;
-			Node next;
-			Node(E v){value=v;}
+	static Edge[][] polygons; // because arrays take less code
+	// Used in polygon simplification.
+	static final double tolerance=1;
 
-			void addAfter(E v){
-				Node next2=next;
-				next=new Node(v);
-				next.next=next2;
-			}
+	static class Edge{
+		// CombinedEdge, but minimized.
+		// Remove information about intermediate points.
+		Point vertex;
+		Color color;
+		Edge(Point v,Color c){
+			vertex=v;color=c;
 		}
-		Node item; // any item. Not guaranteed to be fixed.
-		CircularList(E x){item=new Node(x);item.next=item;}
+	}
 
-		void removeAfter(Node node){
-			// this method must not be provided as a member method
-			// because it may invalidate (item).
-			if(node.next==item)item=node;
-			if(node==node.next)throw new RuntimeException(
-				"Attempt to remove on a singleton circular list");
-			node.next=node.next.next;
+	static class CombinedEdge{
+		// consisting of >=1 pixel edges.
+		List<Point> edges; // including first, excluding last coord.
+		Color color;
+		CombinedEdge(Point p){
+			edges=new ArrayList<>();
+			edges.add(p);
 		}
+		void merge(CombinedEdge e){
+			assert color==e.color;
+			edges.addAll(e.edges);
+		}
+	}
 
-		int computeLength(){
-			int len=0;
-			Node node=item;do{
-				++len;
+	static int simplifyPolygon(CircularList<CombinedEdge> q){
+		int len=q.computeLength();
+		CircularList<CombinedEdge>.Node node=q.item;
+
+		// note: idleCount is reset when edges are merged.
+		findEdgeToMerge:
+		for(int idleCount=0;idleCount<len;++idleCount){
+			if(node.value.color!=node.next.value.color){
 				node=node.next;
-			}while(node!=item);
-			return len;
+				continue findEdgeToMerge;
+			}
+
+			final Point A=node  .value.edges.get(0),
+				B=node.next.next.value.edges.get(0);
+			if(A.equals(B)){ // probably can also use ==
+				throw new RuntimeException(
+					"Invalid polygon. Contains vertex "+A);
+			}
+
+			// for performance reason, node.next.value.edges.get(0)
+			// should be tested first.
+			for(Point x:node.next.value.edges)
+				if(x.distToSegment(A,B)>tolerance){
+					node=node.next;
+					continue findEdgeToMerge;
+				}
+			for(Point x:node.value.edges)
+				if(x.distToSegment(A,B)>tolerance){
+					node=node.next;
+					continue findEdgeToMerge;
+				}
+
+			// can merge it.
+			node.value.merge(node.next.value);
+			q.removeAfter(node);
+			--len;
+			idleCount=0;
 		}
+
+		assert validPolygon(q,len);
+		return len;
 	}
 
-	// Note that (Point) is mutable.
-	static final class Point implements Comparable<Point>{
-		final int x,y;
-		Point(int _x,int _y){x=_x ;y=_y ;}
-
-		Point add(Point p){ return new Point(x+p.x,y+p.y); }
-		Point sub(Point p){ return new Point(x-p.x,y-p.y); }
-		Point mul(int a){ return new Point(x*a,y*a); }
-		Point shr(int a){ return new Point(x>>a,y>>a); }
-
-		Point neg (){ return new Point(-x,-y); }
-		/// Rotate 90° clockwise, assuming +x is →, +y is ↓.
-		Point rt90(){ return new Point(-y, x); }
-		Point lt90(){ return new Point( y,-x); }
-
-		int dot  (Point p){ return x*p.x + y*p.y; }
-		/// Magnitude of the resulting vector when interpret the two
-		/// points as vector in 3D space and take the cross product.
-		int cross(Point p){ return x*p.y - y*p.x; }
-
-		int norm(){ return x*x+y*y; } // dot with itself
-		double dist(Point p){ return Math.sqrt(sub(p).norm()); }
-
-		/// Return the distance from this point to line AB.
-		double distToLine(Point a,Point b){
-			Point ab=b.sub(a);
-			int norm=ab.norm();
-			assert norm>0;
-			return Math.abs(sub(a).cross(ab)/Math.sqrt(norm));
+	static boolean validPolygon(CircularList<CombinedEdge>q,int len){
+		if(len!=q.computeLength())return false;
+		CircularList<CombinedEdge>.Node node=q.item;
+		for(int i=0;i<len;++i){
+			final Point
+				A=node.     value.edges.get(0),
+				B=node.next.value.edges.get(0);
+			for(Point x:node.value.edges)
+				if(x.distToSegment(A,B)>tolerance)return false;
+			node=node.next;
 		}
-
-		/// Return the distance from this point to line segment AB.
-		double distToSegment(Point a,Point b){
-			Point ab=b.sub(a);
-			if(sub(a).dot(ab)<=0)return dist(a);
-			if(sub(b).dot(ab)>=0)return dist(b);
-			return distToLine(a,b);
-		}
-
-		@Override
-		public int compareTo(Point p){
-			return x!=p.x?
-				Integer.compare(x,p.x):Integer.compare(y,p.y);
-		}
-
-		@Override
-		public boolean equals(Object o){
-			if(o==null||!(o instanceof Point))return false;
-			Point p=(Point)o;
-			return x==p.x&&y==p.y;
-		}
-		@Override
-		public int hashCode(){ return x*109+y; }
-		@Override
-		public String toString(){ return "("+x+", "+y+')'; }
+		return node==q.item;
 	}
 
-	static Point[][] polygons; // because arrays take less code
-	// Used in polygon edge detection.
-	static final double tolerance=3;
 
 	static void computePolygons(){
 		Point[] minPoint=new Point[nGroup];
@@ -206,38 +286,41 @@ public class Main{
 		}
 
 		assert polygons==null;
-		polygons=new Point[nGroup][];
+		polygons=new Edge[nGroup][];
 
 		for(int group=0;group<nGroup;++group){
-			CircularList<List<Point>> q;
+			CircularList<CombinedEdge> q;
 
 			// actual processing
-			int len=1;
 			Point position=minPoint[group],
 				direction=new Point(1,0);
-			q=new CircularList<List<Point>>(
-				new ArrayList<Point>(asList(position)));
-			CircularList<List<Point>>.Node node=q.item;
+			q=new CircularList<>(new CombinedEdge(position));
+			CircularList<CombinedEdge>.Node node=q.item;
 
 			while(true){ // traverse the boundary
-				// first, advance the position
+				// save the pixel color
+				Point pixel=position.add(
+					direction.add(direction.rt90()).shr(1)
+				);
+				assert groupOf[pixel.x][pixel.y]==group;
+				node.value.color=data[pixel.x][pixel.y];
+
+				// advance the position
 				position=position.add(direction);
 
 				// check if we finished the cycle
-				if(position.equals(node.next.value.get(0)))break;
+				if(position.equals(node.next.value.edges.get(0)))break;
 
 				// add new position to (q)
-				node.addAfter(
-					new ArrayList<Point>(asList(position)));
+				node.addAfter(new CombinedEdge(position));
 				node=node.next;
-				++len;
 
 				// rotate the direction
 				direction=direction.lt90();
 				while(true){
 					Point dirRT90=direction.rt90();
 					// the pixel the rotation will sweep over
-					Point pixel=position.add(
+					pixel=position.add(
 						direction.add(dirRT90).shr(1)
 					);
 					if(
@@ -249,59 +332,17 @@ public class Main{
 				}
 			}
 
-			// finished the boundary. Now canonicalize it.
-			// note: idleCount is reset when edges are merged.
-			findEdgeToMerge:
-			for(int idleCount=0;idleCount<len;++idleCount){
-				// if(true)break;
+			int len=simplifyPolygon(q);
 
-				final Point A=node  .value.get(0),
-					B=node.next.next.value.get(0);
-				if(A.equals(B)){ // probably can also use ==
-					throw new RuntimeException(
-						"Invalid polygon. Contains vertex "+A);
-				}
-
-				// for performance reason, node.next.value.get(0)
-				// should be tested first.
-				for(Point x:node.next.value)
-					if(x.distToSegment(A,B)>tolerance){
-						node=node.next;
-						continue findEdgeToMerge;
-					}
-				for(Point x:node.value)
-					if(x.distToSegment(A,B)>tolerance){
-						node=node.next;
-						continue findEdgeToMerge;
-					}
-
-				// can merge it.
-				node.value.addAll(node.next.value);
-				q.removeAfter(node);
-				--len;
-				idleCount=0;
-			}
-
-			assert len==q.computeLength();
-			node=q.item;for(int i=0;i<len;++i){
-				final Point
-					A=node.     value.get(0),
-					B=node.next.value.get(0);
-				for(Point x:node.value)
-					assert(x.distToSegment(A,B)<=tolerance);
-				node=node.next;
-			}
-			assert node==q.item;
-
-			// polygon = map(first, q)
-			Point[] polygon=new Point[len];
+			Edge[] polygon=new Edge[len];
 			node=q.item;
 			for(int i=0;i<len;++i,node=node.next)
-				polygon[i]=node.value.get(0);
+				polygon[i]=new Edge(
+					node.value.edges.get(0),
+					node.value.color);
 
 			polygons[group]=polygon;
 		}
-
 	}
 
 	public static void main(String[] args){
@@ -355,7 +396,7 @@ public class Main{
 			int minDist=Integer.MAX_VALUE,minIndex=-1;
 			int color=image.getRGB(x,y);
 			for(int index=0;index<colors.length;++index){
-				int dist=colorDist(color,colors[index].value);
+				int dist=Color.colorDist(color,colors[index].value);
 				if(dist==minDist)
 					minIndex=-1;
 				else if(dist<minDist){
@@ -373,7 +414,7 @@ public class Main{
 				return;
 			}
 			data[x][y]=col;
-			image.setRGB(x,y,makeBrighter(col.value));
+			image.setRGB(x,y,Color.makeBrighter(col.value));
 
 			// Remove comments (but doesn't change the background)
 			if(col==Color.Comment)data[x][y]=Color.Background;
@@ -392,13 +433,13 @@ public class Main{
 			RenderingHints.VALUE_ANTIALIAS_ON:
 			RenderingHints.VALUE_ANTIALIAS_OFF);
 		for(int group=0;group<nGroup;++group){
-			Point[] polygon=polygons[group];
+			Edge[] polygon=polygons[group];
 
 			int len=polygon.length;
 			int[] xs=new int[len],ys=new int[len];
 			for(int i=0;i<len;++i){
-				xs[i]=polygon[i].x;
-				ys[i]=polygon[i].y;
+				xs[i]=polygon[i].vertex.x;
+				ys[i]=polygon[i].vertex.y;
 			}
 			g.drawPolygon(xs,ys,len);
 		}
@@ -407,7 +448,7 @@ public class Main{
 		// Create the window.
 		// Bottleneck 2: Loading JFrame takes ~0.6s
 		JFrame frame=new JFrame();
-		MyJPanel panel=new MyJPanel(image);
+		Panel panel=new Panel(image);
 		frame.add(panel);
 		frame.setSize(400,400);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
